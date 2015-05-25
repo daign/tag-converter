10a4771de9ec11aef21a898b7511c4af9471f5fe
daign <jakob.mischek@daign.de>
2015-05-25 22:15:53 +0200

    using ng-repeat

--- js/tag-converter.js
+++ js/tag-converter.js
@@ -3,6 +3,7 @@ var app = angular.module( 'TagConverter', [] );
 app.controller( 'TagConverterController', function ( $scope ) {
 	$scope.id = 705;
 	$scope.showGrid = false;
+	$scope.digits = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
 	$scope.getBinaryDigit = function ( pos ) {
 		return ( $scope.id & Math.pow( 2, pos ) ) && 1;
 	};
