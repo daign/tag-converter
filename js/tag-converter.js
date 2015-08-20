7d8b7fbbb979aca62dbe03ee926924866ecd6032
daign <jakob.mischek@daign.de>
2015-08-20 11:27:09 +0200

    work in progress: different tag interpretations

--- js/tag-converter.js
+++ js/tag-converter.js
@@ -1,17 +1,26 @@
 var app = angular.module( 'TagConverter', [] );
 
 app.controller( 'TagConverterController', function ( $scope ) {
-	$scope.id = 705;
-	$scope.showGrid = false;
-	$scope.digits = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
-	$scope.getBinaryDigit = function ( pos ) {
-		return ( $scope.id & Math.pow( 2, pos ) ) && 1;
+
+	$scope.digits = [ 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0 ];
+	$scope.flipDigit = function ( index ) {
+		$scope.digits[ index ] = $scope.digits[ index ] ^ 1;
 	};
-	$scope.setBinaryDigit = function ( pos ) {
-		$scope.id = $scope.id ^ Math.pow( 2, pos );
-	};
-	$scope.switchGrid = function () {
+
+	$scope.showGrid = true;
+	$scope.toggleShowGrid = function () {
 		$scope.showGrid = !$scope.showGrid;
 	};
+
+	$scope.readingMode = 1;
+	$scope.setReadingMode = function ( mode ) {
+		$scope.readingMode = mode;
+	};
+
+	$scope.decoderId = 705;
+	$scope.decoderDigits = [ 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ];
+	$scope.preparationId = 101;
+	$scope.preparationDigits = [ 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7 ];
+
 });
 
